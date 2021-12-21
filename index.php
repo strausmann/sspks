2fc76d468f2137a06d8a90f8e4b87230728bd59d
Björn Strausmann <bjoern@strausmann.net>
2021-12-21 17:39:57 +0000

    Update xdebug config

--- index.php
+++ index.php
@@ -15,5 +15,8 @@ $config = new Config(__DIR__, 'conf/sspks.yaml');
 $config->baseUrlRelative = substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['REQUEST_URI'], '/')) . '/';
 $config->baseUrl = 'http' . (array_key_exists('HTTPS', $_SERVER) && $_SERVER['HTTPS']?'s':'') . '://' . $_SERVER['HTTP_HOST'] . $config->baseUrlRelative;
 
+echo $config->baseUrl;
+exit;
+
 $handler = new Handler($config);
 $handler->handle();
